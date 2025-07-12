61bc346bbb7f9db4d56914047b749844d3f06465
RahulSP-4601 <rahulsp4601@gmail.com>
2025-07-11 19:09:54 -0500

    Docker Deployed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    // avoid pre-bundling problems by disabling native esbuild
+    esbuildOptions: {
+      target: 'esnext',
+      supported: {
+        'top-level-await': true
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      esbuild: path.resolve(__dirname, 'node_modules/esbuild-wasm')
+    }
+  }
+});
